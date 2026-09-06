c621f5aac00d05145364e5c0ad97d3ee913bbd88
andrearantes <andre.marcio.ti@gmail.com>
2026-09-06 12:17:52 -0300

    verify task

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -129,6 +129,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
